8762e05a7de0ec1337eac869182b2abca0e7f806
Miguel Correia <geral@nologic.pt>
2021-10-15 16:49:53 +0100

    GGG

--- CNCliente/Models/Tarefa.cs
+++ CNCliente/Models/Tarefa.cs
@@ -15,15 +15,15 @@ namespace CNCliente.Models
         }
 
         [Key]
-        public int tarefaId { get; set; }
-        public int? tarefaParentid { get; set; }
-        public int projeto_Id { get; set; }
+        public Int64 tarefaId { get; set; }
+        public Int64 tarefaParentid { get; set; }
+        public Int64 projeto_Id { get; set; }
         [Required]
         public string tarefaNome { get; set; }
         public DateTime tarefaDatahoraregisto { get; set; }
-        public DateTime? tarefaDatalimite { get; set; }
-        public int? tarefaPosicao { get; set; }
-        public int utilizador_Id { get; set; }
+        public DateTime tarefaDatalimite { get; set; }
+        public Int64 tarefaPosicao { get; set; }
+        public Int64 utilizador_Id { get; set; }
         public bool tarefaEstado { get; set; }
         public bool tarefaTemfilho { get; set; }
 
--- CNCliente/Program.cs
+++ CNCliente/Program.cs
@@ -5,12 +5,12 @@ using CNCliente;
 using Microsoft.Extensions.DependencyInjection;
 using Syncfusion.Blazor;
 
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDk1OTI0QDMxMzkyZTMyMmUzMEIvMGRKbHBqb0hONmhjMnlzVEc2ODZaVTYzcG9CdU9STGR2MDZEMkZ5Um89");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTE5NTUxQDMxMzkyZTMzMmUzMGF5Q0EwNEVBUS9JRGhLYWZIaHh2OVFDaGQrTW1UQWNiTU1mSEZyQ3krNkk9");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:45000") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:45001") });
 
 builder.Services.AddSyncfusionBlazor();
 
